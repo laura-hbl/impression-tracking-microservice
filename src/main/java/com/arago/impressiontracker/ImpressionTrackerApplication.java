bdd6a287ddb2da7de2bcfc86820b2411fc6544c1
Laura <laura_habdul@hotmail.fr>
2024-10-16 15:23:36 +0200

    refactor: rename application to ImpressionTrackerApplication

--- src/main/java/com/arago/impressiontracker/ImpressionTrackerApplication.java
+++ src/main/java/com/arago/impressiontracker/ImpressionTrackerApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ImpressiontrackerApplication {
+public class ImpressionTrackerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ImpressiontrackerApplication.class, args);
+		SpringApplication.run(ImpressionTrackerApplication.class, args);
 	}
 
 }
--- src/test/java/com/arago/impressiontracker/ImpressionTrackerApplicationTests.java
+++ src/test/java/com/arago/impressiontracker/ImpressionTrackerApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ImpressiontrackerApplicationTests {
+class ImpressionTrackerApplicationTests {
 
 	@Test
 	void contextLoads() {
