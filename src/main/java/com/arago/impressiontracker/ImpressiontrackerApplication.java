3869619034a843c7ec5e3199538b971143f112e8
Laura <laura_habdul@hotmail.fr>
2024-10-16 15:21:52 +0200

    Initial commit

---
+++ src/main/java/com/arago/impressiontracker/ImpressiontrackerApplication.java
@@ -0,0 +1,13 @@
+package com.arago.impressiontracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImpressiontrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImpressiontrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/arago/impressiontracker/ImpressiontrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arago.impressiontracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImpressiontrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
